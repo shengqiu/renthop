5a5e1a1481428865db739ee9157a6dba8122d60e
shengqiu <shengqiu.purdue@gmail.com>
2017-04-08 13:38:34 -0400

    to black mac

--- dbFunctions.py
+++ dbFunctions.py
@@ -1,4 +1,5 @@
 from pymongo import MongoClient
+import json
 
 
 def insertOne(item):
@@ -7,5 +8,16 @@ def insertOne(item):
   collection = db.test_collection
   collection.insert(item)
   for one in collection.find():
-    print item
-  
+    print 'there are {} of records.'.format(len(one))
+  return
+
+
+if __name__ == '__main__':
+  result = json.load(open('test.json', 'rb'))
+  numOfRecord = len(result['listing_id'])
+  colNames = result.keys()
+  for i in range(numOfRecord):
+    temp = {}
+    for colName in colNames:
+      temp[colName] = result[colName][i]
+    insertOne(temp)
